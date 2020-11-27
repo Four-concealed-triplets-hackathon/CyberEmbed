02fdb884fa880e9948c41574e0cdc6c1e2febe2e
cilebritain <2023697797@qq.com>
2020-11-28 02:06:55 +0800

    spider demo done
    add spider demo

--- src/service/SpiderService.java
+++ src/service/SpiderService.java
@@ -1,4 +1,24 @@
 package service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class SpiderService {
+    public static void getEmbed(String url){
+        try {
+            Document document = Jsoup.connect(url).get();
+            // github url
+            if (url.contains("github.com")) {
+                String author = document.select("[itemprop=author]").text();
+                String title = document.select("[itemprop=name]").text();
+                System.out.println(author);
+                System.out.println(title);
+            }
+        } catch (Exception e){
+            System.out.println("get html content error");
+        }
+    }
+//    public static void main(String[] args) {
+//        getEmbed("https://github.com/jhy/jsoup");
+//    }
 }
