98f99d677cf9227f04b567a25f34c9873bf1b0b0
CappuccinoCup <CappuccinoCup@126.com>
2020-11-28 04:02:12 +0800

    easy poster demo done

--- src/service/PosterService.java
+++ src/service/PosterService.java
@@ -5,38 +5,89 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
 
 public class PosterService {
     /**
-     * 生成Github项目海报
-     *
-     * @author cappuccinocup
+     * generate poster for Github repository
      */
-    private static BufferedImage generateGithubPoster(String title, String[] authors, String desc, int star, int fork) {
-        BufferedImage poster = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
-        // TODO:
+    private static BufferedImage generateGithubPoster(String url, String title, String authors, String desc, int star, int fork) throws IOException {
+        BufferedImage poster = new BufferedImage(1000, 600, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = poster.createGraphics();
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 500, 500);// 填充整个屏幕
 
-        g.setFont(new Font("Microsoft YaHei", Font.BOLD, 20));
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        // fill background
+        BufferedImage bg = ImageIO.read(new File("./public/img/dark_bg.jpg"));
+        g.drawImage(bg.getScaledInstance(bg.getWidth(), bg.getHeight(), Image.SCALE_SMOOTH), 0, 0, 1000, 600, null);
+
+        // add title information
+        g.setFont(new Font("Microsoft YaHei", Font.BOLD, 40));
         g.setColor(new Color(255, 255, 255));
-        g.drawString(title, 50, 20);
+        g.drawString(title, 60, 80);
 
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);// 消除画图锯齿
-        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);// 消除文字锯齿
+        // add other information
+        g.setFont(new Font("Microsoft YaHei", Font.PLAIN, 30));
+        g.setColor(new Color(255, 255, 255));
+        g.drawString("Authors: " + authors, 60, 180);
+        g.drawString("Description: " + desc, 60, 230);
+        g.drawString("stars: " + star, 60, 500);
+        g.drawString("forks: " + fork, 240, 500);
+
+        // create qr code
+        BufferedImage qrCode = createQrCode(url, 200, 200);
+        g.drawImage(qrCode.getScaledInstance(qrCode.getWidth(), qrCode.getHeight(), Image.SCALE_SMOOTH), 750, 40, 200, 200, null);
+
+        // release resource
         g.dispose();
 
         return poster;
     }
 
-    public static void main(String[] args) {
-        String outputPath = "./poster.jpg";
-        BufferedImage poster = generateGithubPoster("four-concealed-triplets-hackathon",
-                new String[]{"cli", "FLL", "Ruku", "cap"},
-                "Hackathon2020@eastchina，Team Four Concealed Triplets. A website that can embed website content into a simple image (supporting Github, CNKI, Twitter, Spotify)",
-                100, 5);
+    private static final int BLACK = 0xFF000000;
+    private static final int WHITE = 0xFFFFFFFF;
+
+    public static BufferedImage createQrCode(String url, int width, int height) {
         try {
+            Map<EncodeHintType, String> hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height, hints);
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    image.setRGB(x, y, bitMatrix.get(x, y) ? BLACK : WHITE);
+                }
+            }
+            return image;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * an easy poster demo
+     */
+    public static void main(String[] args) {
+
+        String outputPath = "./public/img/poster.jpg";
+
+        try {
+            BufferedImage poster = generateGithubPoster(
+                    "https://github.com/CappuccinoCup/MyChair",
+                    "MyChair",
+                    "CappuccinoCup",
+                    "vue-cli3 front end for 2020 SE",
+                    0,
+                    0
+            );
             ImageIO.write(poster, "jpg", new File(outputPath));
         } catch (IOException e) {
             e.printStackTrace();
