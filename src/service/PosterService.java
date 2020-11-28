ee56dda0d448fbd285e4fee31032f5da3644c47a
CappuccinoCup <CappuccinoCup@126.com>
2020-11-28 17:26:19 +0800

    embed v0.1

--- src/service/PosterService.java
+++ src/service/PosterService.java
@@ -7,12 +7,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class PosterService {
     /**
@@ -26,7 +26,7 @@ public class PosterService {
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
 //        // fill background
-//        BufferedImage bg = ImageIO.read(new File("./public/img/dark_bg.jpg"));
+//        BufferedImage bg = ImageIO.read(new File("./public/img/git_bg.jpg"));
 //        g.drawImage(bg.getScaledInstance(bg.getWidth(), bg.getHeight(), Image.SCALE_SMOOTH), 0, 0, 1000, 600, null);
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 1000, 600);
@@ -69,7 +69,7 @@ public class PosterService {
         g.drawString("forks: " + fork, 240, 500);
 
         // create qr code
-        BufferedImage qrCode = createQrCode(url, 200, 200);
+        BufferedImage qrCode = createQrCode(url, 300, 300, "./public/img/github_logo.png");
         g.drawImage(qrCode.getScaledInstance(qrCode.getWidth(), qrCode.getHeight(), Image.SCALE_SMOOTH), 750, 40, 200, 200, null);
 
         // release resource
@@ -81,17 +81,38 @@ public class PosterService {
     private static final int BLACK = 0xFF000000;
     private static final int WHITE = 0xFFFFFFFF;
 
-    public static BufferedImage createQrCode(String url, int width, int height) {
+    public static BufferedImage createQrCode(String url, int width, int height, String logoPath) {
         try {
-            Map<EncodeHintType, String> hints = new HashMap<>();
+            HashMap hints = new HashMap();
             hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+            hints.put(EncodeHintType.MARGIN, 1);
             BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height, hints);
             BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            Image logo = ImageIO.read(new File(logoPath));
+            int logoWidth = Math.min(logo.getWidth(null), image.getWidth() * 2 / 10);
+            int logoHeight = Math.min(logo.getHeight(null), image.getHeight() * 2 / 10);
+            int logoX = (image.getWidth() - logoWidth) / 2;
+            int logoY = (image.getHeight() - logoHeight) / 2;
+
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
                     image.setRGB(x, y, bitMatrix.get(x, y) ? BLACK : WHITE);
                 }
             }
+
+            Graphics2D graphics = image.createGraphics();
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            graphics.drawImage(logo, logoX, logoY, logoWidth, logoHeight, null);
+
+            graphics.setStroke(new BasicStroke(5));
+            graphics.setColor(Color.WHITE);
+            graphics.drawRect(logoX, logoY, logoWidth, logoHeight);
+
+            graphics.dispose();
+
             return image;
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,14 +125,14 @@ public class PosterService {
      */
     public static void main(String[] args) {
 
-        String outputPath = "./public/img/poster.jpg";
+        String outputPath = "./public/img/embed.jpg";
 
         try {
             BufferedImage poster = generateGithubPoster(
                     "https://github.com/CappuccinoCup/MyChair",
                     "MyChair",
                     "CappuccinoCup",
-                    "vue-cli3 front end for 2020 SEE and vue-cli3 front end for 2020 SEE and vue-cli3 front end for 2020 SEE",
+                    "vue-cli3 front end for 2020 SE, more words needed more words needed more words needed more words needed more words needed more words needed more words needed more words needed more words needed more words needed...",
                     "0",
                     "0"
             );
